ea4630d11e8bdde7a40f7c81142ee942ecb43605
Alex Leemon <a.leemon7@gmail.com>
2022-03-10 15:40:32 +1100

    Further updates, still needs some serious optimisation improvements

--- 001_spellingbee.R
+++ 001_spellingbee.R
@@ -16,11 +16,17 @@
   # Make it fun and enter the letters in a hexagon? (will require advanced CSS)
   # Show three levels of hints:
     # Number of words, by letter count
+    # Show the number of pangrams
     # Number of words by starting letters
     # Full word list
 
 ## If it takes forever to compute, might need ot pre-compute?
 
+
+# Ability to show historical data? Like store previously days etc.
+
+
+#~ Testing on 9-Mar-22 4 letter words indicates that a lot of the Scrabble words are 
 
 
 
@@ -30,17 +36,21 @@ library(tidyverse)
 library(words)
 #library(combinat) # Not using this package
 library(gtools)
+library(stringi)
 
 
 
 ##-----  3. Define Permutation and Search Function  -----
 
+
+
 spelling_bee <- function(n, spb_letters, spb_centre) {
-  
+
+  system.time({  
   ## Get all permutations, with repeats for n letter words
   perms <- permutations(
     7, # 7 input letters
-    n, # Seeking words of length num
+    n, # Seeking words of length n
     spb_letters, # Input of the Spelling Bee letters
     set = FALSE, # Don't remove repeats from source vector, probably not needed
     repeats.allowed = TRUE # Yes, definitely want repeats of letters
@@ -48,15 +58,32 @@ spelling_bee <- function(n, spb_letters, spb_centre) {
     as.data.frame() %>% # Convert to data frame
     unite(col = 'merged', sep = "") %>% # Concatenate the columns to form words
     pull() # Convert to atomic vector
-  
+  }) 
   
   
   ## Drop rows which don't feature the centre letter
   perms <- perms[str_detect(perms, spb_centre)]
   
   
+  #~ Drop any words where a single letter appears more than twice consecutively
+    #~ Three consectuive letters might exist, if a hyphen is not accounted for, or
+    #~ Onomaetopoes (bzzz, aaargh, etc.) But these are unlikely to be actual words
+    #~ Validate this theory by searching the words database for three consecutive letters
+  
+  perms <- perms[str_detect(perms, pattern = str_c(spb_centre, "{3,}"), negate = TRUE)] 
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[1], "{3,}"), negate = TRUE)]
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[2], "{3,}"), negate = TRUE)]
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[3], "{3,}"), negate = TRUE)]
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[4], "{3,}"), negate = TRUE)]
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[5], "{3,}"), negate = TRUE)]
+  perms <- perms[str_detect(perms, pattern = str_c(spb_letters[6], "{3,}"), negate = TRUE)]
+  
+  #~ This is good, but still too many permutations of junk combos
   
   
+  
+  
+  system.time({   
   ## Check all permutations against the valid words list
   acceptable_words <- list()
   
@@ -64,16 +91,24 @@ spelling_bee <- function(n, spb_letters, spb_centre) {
     
     
     ## Filter Scrabble words by desired length to trim the search time
-    scrabble_words <- filter(words, word_length == n)$word
+    word_list <- filter(words, word_length == n)$word
     
     ## Determine if they're in the Scrabble words, if so extract them
-    if(any(str_detect(perms[i], scrabble_words))) {
-      
-      acceptable_words[[i]] <- perms[i]
-      
-    }
+
+    #~ This should speed things up a bit, hopefully?
+    acceptable_words[[i]] <- stri_detect_fixed(perms[i], word_list, max_count = 1)
+    
+    
+    #if(any(str_detect(perms[i], scrabble_words))) {
+    # if(any(stri_detect_fixed(perms[i], scrabble_words, max_count = 1))) {
+    #   
+    #   acceptable_words[[i]] <- perms[i]
+    #   
+    # }
     
   }
+  
+  })
   
   ## Drop the empty elements of the list
   acceptable_words <- acceptable_words[-which(sapply(acceptable_words, is.null))]
@@ -94,6 +129,10 @@ spelling_bee <- function(n, spb_letters, spb_centre) {
 data("words")
 
 
+## Drop any words < 4 letters long
+words <- filter(words, word_length == n)
+
+
 
 ##-----  5. Spelling Bee Letters  ----- 
 
@@ -108,7 +147,7 @@ spb_090322_centre <- 'm'
 ##-----  5. Find the Permutations  -----
 
 #~ Longest word in English is 45 letters long
-  #! Realistically, how many iterations should I bother running through?
+  #! Realistically, how many iterations should I bother running through, probably 12 or 14
 
 
 answers <- map(c(4:12), function(n) {spelling_bee(n, spb_090322, spb_090322_centre)})
@@ -122,8 +161,55 @@ answers <- map(c(4:12), function(n) {spelling_bee(n, spb_090322, spb_090322_cent
   # Remove 'words which have a high degree of repeat of the same letter (regardless of consecutive-ness)
 
 # Test where the bottlenecks are - in the permutation generation phase, or the matching phase?
+  #~ The bottleneck is in the matching words against the scrabble list
+  #~ But the permutation calculation also blows out badly with longer words
+
+# Introduce DT?
+  # https://stackoverflow.com/questions/63120555/how-to-optimize-string-detection-for-speed
+
+
+## This SO thread has some interesting approaches:
+  # https://stackoverflow.com/questions/54717711/efficient-pattern-detecting-in-vector-of-strings
+  # It suggests stringi package is faster than the stringr package?
+  # Also suggests that base grepl() is faster than str_detect()
+
+
+## Some analysis of bigram frequency in English:
+  # https://www.petercollingridge.co.uk/blog/language/analysing-english/bigrams/
 
 
 
+
+## Package 'ri' might be able to provide some assistance for breaking down the permutation problem?
+
+
+
+##--- Testing
+
+## n = 4
+  # Permutation = 0 seconds
+  # Matching = 53 seconds
+
+
+## n = 6
+  # Permutation = .21 seconds
+  # No. of perms = 117,649
+  # Matching = takes way too long (several minutes at least)
+  # After dropping 3 letter combos = 109,116 long. Still too long
+  # Whole function took 6.5 minutes to resolve the answers - clearly the problem is increasing exponentially
+
+
+## n = 12
+  # Permutation failed with a 2.7 GB vector (!!)
+  # Permutations = 7^12 = 13,841,287,201 (which is should be able to handle, it's just being a lil' bitch)
+  # It's gonna struggle with permutations above 12 letter words
+
+
+
+
+
+system.time({
+  answers <- spelling_bee(6, spb_090322, spb_090322_centre)
+})
 
 
---
+++ 002_answer_database.R
@@ -0,0 +1,42 @@
+##-----  1. Overview  -----
+
+# Provide two things in this script:
+  # 1. A list of the historical puzzle questions
+  # 2. A list of the historical answer sets
+
+
+## Use these answer sets to compare against the Scrabble dictionary
+  # There seem to be a lot of words not allowed by Spelling Bee
+  # This might be more prevalent for the shorter words?
+
+
+# Look online for historical answers too?
+
+
+## Export data to a database
+
+
+##-----  2. Packages  -----
+
+library(tidyverse)
+
+
+
+##-----  3. Data  -----
+
+##--- 9 March 2022
+
+spb_090322 <- c('a','i', 't', 'r', 'c', 'o', 'm') 
+spb_090322_centre <- 'm'
+
+## Keep two lists:
+  # Found answers (i.e. Scrabble answers)
+  # Spelling Bee answers (the actual answers)
+  # Cross compare the list over time to filter it down?
+
+
+spb_090322_answers <- c()
+
+
+
+
